801b3f386450ec340d9e11b986516ac915af461b
javier@hodl.nl <javier@hodl.nl>
2024-05-31 11:38:40 +0200

    React query function to avoid refetching when reopening browser's window

--- lib/QueryProvider.tsx
+++ lib/QueryProvider.tsx
@@ -3,7 +3,13 @@ import React, { ReactNode, useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export const QueryProvider: React.FC<{children: ReactNode}> = ({ children }) => {
-  const [ queryClient ] = useState(() => new QueryClient())
+  const [ queryClient ] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false, // React query function to avoid refetching when reopening browser's window 
+      },
+    }
+  }))
   
   return <QueryClientProvider client={queryClient}>{ children }</QueryClientProvider>
 }
